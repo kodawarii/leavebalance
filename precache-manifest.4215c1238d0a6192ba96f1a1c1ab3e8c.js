7d7308641a6e4ead15c760b7bb726eb45ef3fd78
eunbalman <bama_boy2@hotmail.com>
2019-03-21 11:50:41 +1300

    Updates

---
+++ precache-manifest.4215c1238d0a6192ba96f1a1c1ab3e8c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/leavebalance/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "461ed635700a9110a8ce",
+    "url": "/leavebalance/static/js/runtime~main.b41e82a1.js"
+  },
+  {
+    "revision": "8f57c70246dcddd1ecbd",
+    "url": "/leavebalance/static/js/main.eee519f1.chunk.js"
+  },
+  {
+    "revision": "ba264cd90c4d36546d5a",
+    "url": "/leavebalance/static/js/2.8f71a45e.chunk.js"
+  },
+  {
+    "revision": "8f57c70246dcddd1ecbd",
+    "url": "/leavebalance/static/css/main.a3a46ca6.chunk.css"
+  },
+  {
+    "revision": "fd943f8111131321eeb882ae6a0e858c",
+    "url": "/leavebalance/index.html"
+  }
+];
