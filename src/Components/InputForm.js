cdf8f7bdb0faebf23043f1521d0127a89e6158b5
eunbalman <bama_boy2@hotmail.com>
2019-03-22 16:27:04 +1300

    Beta Version Commit as of 03/22/2019 Final Day At B:Drive && Final Deploy

--- src/Components/InputForm.js
+++ src/Components/InputForm.js
@@ -17,6 +17,10 @@ class InputForm extends Component {
         }
     }
 
+    setG10Date(){
+        this.setState({join: "16/07/2018"});
+    }
+
     handleJoinChange(e){
         this.state.join = e.target.value;
     }
@@ -57,24 +61,52 @@ class InputForm extends Component {
     return (
       <div className="inputform">
           <form onSubmit={this.handleSubmit.bind(this)}>
-            <ul>
-                <li>
-                    Join Date: <br/><br/>
-                    <input type="date" onChange={this.handleJoinChange.bind(this)} className="inputField"/>
-                </li>
 
-                <li>
-                    Leaves Used: <br/><br/>
-                    <input type="text" maxlength="3" placeholder="From 0 to 999 (Days)" pattern="\d*" onChange={this.handleUsedChange.bind(this)} className="inputField"/>
-                </li>
+            <table>
+                <tbody>
+                    <tr>
+                        <td>
+                            <ul>
+                                <li>
+                                    Join Date: <br/><br/>
+                                    <input type="date" onChange={this.handleJoinChange.bind(this)} className="inputField"/> <br/><br/>
+                                    <div className="gradButtonContainer">
+                                        <span className = "gradButtonWrapper"><span className= "gradButton"> G1 </span></span>
+                                        <span className = "gradButtonWrapper"><span className= "gradButton"> G2 </span></span>
+                                        <span className = "gradButtonWrapper"><span className= "gradButton"> G3 </span></span>
+                                        <span className = "gradButtonWrapper"><span className= "gradButton"> G4 </span></span>
+                                        <span className = "gradButtonWrapper"><span className= "gradButton"> G5 </span></span>
+                                        <span className = "gradButtonWrapper"><span className= "gradButton"> G6 </span></span>
+                                    </div>
+                                    <div className="gradButtonContainer">
+                                        <span className = "gradButtonWrapper"><span className= "gradButton"> G7 </span></span>
+                                        <span className = "gradButtonWrapper"><span className= "gradButton"> G8 </span></span>
+                                        <span className = "gradButtonWrapper"><span className= "gradButton"> G9 </span></span>
+                                        <span className = "gradButtonWrapper"><span className= "gradButton" onClick={this.setG10Date.bind(this)}> G10 </span></span>
+                                        <span className = "gradButtonWrapper"><span className= "gradButton"> G11 </span></span>
+                                    </div>
+                                </li>
 
-                <li>
-                    Forecast Date: <br/><br/>
-                    <input type="date" onChange={this.handleForecastChange.bind(this)} defaultValue={date} className="inputField"/>
-                </li>
-            </ul>
-            
-            <input id="submitButton" type="submit" value="Calculate Forecast" className="submitButton" />
+                                <li>
+                                    Leaves Used: <br/><br/>
+                                    <input type="text" maxlength="3" placeholder="From 0 to 999 (Days)" pattern="\d*" onChange={this.handleUsedChange.bind(this)} className="inputField"/>
+                                </li>
+
+                                <li>
+                                    Forecast Date: <br/><br/>
+                                    <input type="date" onChange={this.handleForecastChange.bind(this)} defaultValue={date} className="inputField"/>
+                                </li>
+                            </ul>
+                        </td>
+                    </tr>
+                    
+                    <tr>
+                        <td>
+                            <input id="submitButton" type="submit" value="Go!" className="submitButton" /> 
+                        </td>
+                    </tr>
+                </tbody>
+            </table>
 
           </form>
       </div>
